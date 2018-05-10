af16b934e40fbd331c9ac6859bf31e137105c40b
Yuqing Lee <im.yuqlee@gmail.com>
2018-05-10 21:45:28 +0800

    Add terms and privacy pages

--- src/page/index.js
+++ src/page/index.js
@@ -4,11 +4,21 @@ import $ from 'jquery';
 function Page(router) {
   this.router = router;
   this.Error404 = require('../404.html');
+  this.templatePrivacy = require('./privacy.html');
+  this.templateTerms = require('./terms.html');
 }
 
 Page.prototype = {
   show: function (id) {
     const self = this;
+    if (id === "terms") {
+      self.terms();
+      return
+    }
+    if (id === "privacy") {
+      self.privacy();
+      return
+    }
     try {
       var layout = require('./' + id + '.' + i18n.locale + '.html');
       $('body').attr('class', 'page layout');
@@ -24,7 +34,25 @@ Page.prototype = {
       $('#layout-container').html(self.Error404());
       self.router.updatePageLinks();
     }
-  }
+  },
+
+  terms: function () {
+    const self = this;
+    $('body').attr('class', 'page layout');
+    $('#layout-container').html(self.templateTerms({
+      logoURL: require('../home/logo.png')
+    }));
+    self.router.updatePageLinks();
+  },
+
+  privacy: function () {
+    const self = this;
+    $('body').attr('class', 'page layout');
+    $('#layout-container').html(self.templatePrivacy({
+      logoURL: require('../home/logo.png')
+    }));
+    self.router.updatePageLinks();
+  },
 };
 
 export default Page;
