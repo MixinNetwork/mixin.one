d9f281628b04ce7e15702fff68965ca58493b8c9
Cedric Fung <cedric@vec.io>
2019-05-20 22:24:46 +0800

    fix deploy script for directory

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rm dist/*
+rm -r dist/*
 npm run dist || exit
 
 gsutil defacl ch -u AllUsers:R gs://mixin-one
@@ -9,7 +9,7 @@ gsutil -h "Cache-Control:public, max-age=31536000" -h "Content-Encoding:gzip" -m
 gsutil -h "Cache-Control:public, max-age=31536000" -h "Content-Encoding:gzip" -m cp dist/*.js gs://mixin-one/assets
 rm dist/*.css dist/*.js
 
-gsutil -h "Cache-Control:public, max-age=31536000" -m cp dist/* gs://mixin-one/assets
+gsutil -h "Cache-Control:public, max-age=31536000" -m cp -r dist/* gs://mixin-one/assets
 
 scp dist/index.html one@mixin-deploy-web:tmp/mixin.one.html
 ssh one@mixin-deploy-web sudo mv /home/one/tmp/mixin.one.html /var/www/html/dist/mixin.one.html
