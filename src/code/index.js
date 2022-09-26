2dde74de5ed9c8f8ffb2988472644d312e75f88a
hundredark <hundredark@gmail.com>
2022-09-26 18:17:16 +0800

    Add chat

--- src/code/index.js
+++ src/code/index.js
@@ -5,8 +5,11 @@ import QRious from 'qrious';
 function Code(router, api) {
   this.router = router;
   this.api = api;
-  this.templateUser = require('./user.html');
   this.templateGroup = require('./group.html');
+  this.templateChat = require('./chat.html');
+
+  this.chatType = ['user', 'conversation'];
+  this.paymenType = 'payment';
 }
 
 Code.prototype = {
@@ -17,35 +20,35 @@ Code.prototype = {
         return;
       }
       resp.data['code_id'] = id;
-      switch (resp.data.type) {
-        case 'user':
-          self.renderUser(resp.data);
-          break;
-        case 'conversation':
-          self.renderGroup(resp.data);
-          break;
-        default:
-          self.api.error({error: {code: 10002}});
+
+      if (self.chatType.includes(resp.data.type)) {
+        self.renderChat(resp.data);
+        return;
       }
+
+      if (this.paymenType === resp.data.type) {
+        self.renderGroup(resp.data);
+        return;
+      }
+
+      self.api.error({error: {code: 10002}});
     }, id);
   },
 
-  renderUser: function(user) {
+  renderChat: function(chatInfo) {
     const self = this;
-    $('body').attr('class', 'user code layout');
-    user['hasAvatar'] = user.avatar_url !== '';
-    user['firstLetter'] = user.full_name.trim()[0] || '^_^';
-    user['logoURL'] = require('../home/logo.png').default;
-    user['full_name'] = user.full_name.trim().length > 0 ? user.full_name.trim() : '^_^';
-    $('#layout-container').html(self.templateUser(user));
-    new QRious({
-      element: document.getElementById('mixin-code'),
-      backgroundAlpha: 0,
-      foreground: '#00B0E9',
-      value: 'https://mixin.one/codes/' + user.code_id,
-      level: 'H',
-      size: 500
-    });
+    $('body').attr('class', 'chat code layout');
+    chatInfo['chatType'] = chatInfo.type === 'conversation' ? 'conversation' : chatInfo.app ? 'bot' : 'user';
+    chatInfo['hasAvatar'] = chatInfo.avatar_url !== '';
+    const full_name = chatInfo.type === 'conversation' ? chatInfo.name : chatInfo.full_name;
+    chatInfo['firstLetter'] = full_name.trim()[0] || '^_^';
+    chatInfo['logoURL'] = require('../home/logo.png').default;
+    chatInfo['full_name'] = full_name.trim().length > 0 ? full_name.trim() : '^_^';
+    chatInfo['info'] = chatInfo.type === 'conversation' ? `${chatInfo.participants.length} members` : chatInfo.identity_number;
+    chatInfo['intro'] = chatInfo.type === 'conversation' ? chatInfo.announcement : chatInfo.biography;
+    chatInfo['actionText'] = chatInfo.type === 'conversation' ? 'Join Group' : 'Chat';
+    chatInfo['mixinUrl'] = "mixin://codes/" + chatInfo.code_id;
+    $('#layout-container').html(self.templateChat(chatInfo));
     self.router.updatePageLinks();
   },
 
