dd3282c296c7337e131765f5d3034a1d7d097bf3
Cedric Fung <cedric@vec.io>
2022-10-26 16:16:52 +0000

    fix github action for webp

--- ci.sh
+++ ci.sh
@@ -2,6 +2,6 @@
 
 mkdir -p ./dist/assets
 
-mv ./dist/{*.js,*.css,*.svg,*.png,*.jpg,*.woff,*.woff2,*.eot,*.ttf,*.gif} ./dist/assets
+mv ./dist/{*.js,*.css,*.svg,*.png,*.jpg,*.woff,*.woff2,*.eot,*.ttf,*.gif,*.webp} ./dist/assets
 mv ./dist/icons ./dist/assets/icons
 
