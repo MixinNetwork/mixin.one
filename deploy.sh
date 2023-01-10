1d6e38e9428e9fe6ca769f50efa75673da0efade
Cedric Fung <cedric@vec.io>
2023-01-10 09:03:56 +0000

    update deeplink files

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ rm dist/*.css dist/*.js
 
 gsutil -h "Cache-Control:public, max-age=31536000" -m cp -r dist/* gs://mixin-one/assets
 
-gsutil -h "Cache-Control:public, max-age=31536000" -m cp .well-known/* gs://mixin-one/.well-known
+gsutil -h "Cache-Control:public, max-age=3600" -h "Content-Type:application/json" -m cp .well-known/* gs://mixin-one/.well-known
 
 scp dist/index.html one@mixin-deploy-web:tmp/mixin.one.html
 ssh one@mixin-deploy-web sudo mv /home/one/tmp/mixin.one.html /var/www/html/dist/mixin.one.html
