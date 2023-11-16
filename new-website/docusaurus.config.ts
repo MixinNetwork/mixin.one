85c99a3ca81d2840aab60879f1ddc95f7c16de3a
YeungKC <I@YeungKC.com>
2023-11-16 12:35:08 +0900

    get started link

--- new-website/docusaurus.config.ts
+++ new-website/docusaurus.config.ts
@@ -85,7 +85,7 @@ const config: Config = {
         },
         {
           label: "Get Started",
-          to: "/",
+          to: "/price",
           position: "right",
           className: "px-6 py-3 dark:bg-white dark:text-zinc-800 bg-zinc-800 text-white rounded font-medium leading-none",
         },
