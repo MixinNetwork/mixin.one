e4bb4fce0efa7d7b13dcee947292af6fbc5e3dfd
YeungKC <I@YeungKC.com>
2023-11-20 17:30:42 +0900

    ui

--- new-website/src/components/common/GetStartedSection.tsx
+++ new-website/src/components/common/GetStartedSection.tsx
@@ -8,15 +8,15 @@ export const GetStartedSection = () => (
     <img src={useBaseUrl("/img/common/getStarted.svg")} className="-z-10 object-cover fill" />
     <div className="pt-20 pb-30 px-10 grid md:grid-cols-2 container mx-auto gap-x-30">
       <img src={require("@site/static/img/common/getStartedPerson.webp").default} alt={"try safe"} className="aspect-355/204 md:aspect-657/376 md:order-last" />
-      <div className="md:flex md:flex-col md:justify-between px-10 md:px-0">
+      <div className="md:flex md:flex-col px-10 md:px-0">
         <div className="mt-17.5 md:mt-0 md:w-fit text-white text-3xl font-medium leading-10">
           <Translate>Get Started</Translate>
         </div>
-        <div className="mt-7 md:mt-0 md:w-fit text-white text-sm font-normal leading-normal">
+        <div className="mt-7 md:w-fit text-white text-sm font-normal leading-normal">
           <Translate>Experience the latest innovations in Bitcoin industry and secure your Bitcoin starting with just $10 per year!</Translate>
         </div>
         <LocalLink
-          className="mt-7 md:mt-0 w-fit py-4 px-7 bg-white bg-opacity-95 rounded-sm flex-center text-zinc-800 text-base font-medium leading-none"
+          className="mt-7 md:mt-auto w-fit py-4 px-7 bg-white bg-opacity-95 rounded-sm flex-center text-zinc-800 text-base font-medium leading-none"
           href="https://calendly.com/mixin-safe/30min?month=2023-11"
         >
           <Translate>Schedule a demo</Translate>
--- new-website/src/components/page/about/CryptoEaseSection.tsx
+++ new-website/src/components/page/about/CryptoEaseSection.tsx
@@ -4,11 +4,11 @@ import Translate from "@docusaurus/Translate"
 
 const Item = ({ title, description }: { title: React.ReactNode; description: React.ReactNode }) => (
   <>
-    <div>
+    <div className="md:max-w-65">
       <div className="text-center text-neutral-800 text-3xl font-medium">{title}</div>
       <div className="mt-4 text-center text-zinc-800 text-opacity-80 text-sm font-normal leading-snug">{description}</div>
     </div>
-    <div className="my-10 mx-auto w-28 h-px bg-zinc-800 bg-opacity-30 sm:my-0 sm:w-px sm:h-28 sm:my-auto sm:last:hidden sm:mx-10 md:mx-20"></div>
+    <div className="my-10 mx-auto w-28 h-px bg-zinc-800 bg-opacity-30 sm:my-0 sm:w-px sm:h-28 sm:my-auto sm:last:hidden sm:mx-10 md:mx-15"></div>
   </>
 )
 
@@ -18,7 +18,7 @@ export const CryptoEaseSection = () => {
       <SectionTitle description={<Translate>We go out of our way to ensure that you have the utmost privacy.</Translate>}>
         <Translate>We believe hold crypto shouldn't be so hard and expensive</Translate>
       </SectionTitle>
-      <div className="mt-5 px-10 grid pb-30 sm:pb-40 sm:grid-cols-[1fr_auto_1fr_auto_1fr]">
+      <div className="mt-5 mx-auto px-10 grid pb-30 sm:pb-40 sm:grid-cols-[1fr_auto_1fr_auto_1fr]">
         <Item
           title={<Translate>2017</Translate>}
           description={
