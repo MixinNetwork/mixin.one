d19e6920fb8216589ad6828a3ae03d38629aafbe
YeungKC <I@YeungKC.com>
2023-11-21 16:15:57 +0900

    Update custom.css and Navbar component

--- new-website/src/theme/Navbar/index.tsx
+++ new-website/src/theme/Navbar/index.tsx
@@ -7,6 +7,7 @@ import clsx from "clsx"
 import { NavbarItem, useThemeConfig } from "@docusaurus/theme-common"
 import { splitNavbarItems } from "@docusaurus/theme-common/internal"
 import useBaseUrl from "@docusaurus/useBaseUrl"
+import Arrow from "@site/static/img/common/nav/arrow.svg"
 
 const Item = (data: NavbarItem) => {
   if (data.items?.length) {
@@ -42,6 +43,41 @@ const Item = (data: NavbarItem) => {
   return <Link {...data}>{data.label}</Link>
 }
 
+const MobileItem = (data: NavbarItem) => {
+  const defaultClassName = "text-zinc-800 text-sm font-normal"
+  if (data.items?.length) {
+    const id = "nav-" + data.label
+    return (
+      <div>
+        <input id={id} type="checkbox" className="hidden peer" />
+        <label htmlFor={id} className={clsx("group py-8 px-6 flex flex-row justify-between peer-checked:svg:-rotate-180")}>
+          <div className={clsx("group-hover:text-zinc-200", defaultClassName)}>{data.label}</div>
+          <Arrow className="transition-all duration-300" />
+        </label>
+
+        <div className="grid grid-rows-[0fr] transition-all duration-500 peer-checked:grid-rows-[1fr] peer-checked:children:border-t-1">
+          <div className="overflow-hidden">
+            <div className="text-zinc-800 text-opacity-70 text-xs font-normal uppercase pb-5 px-6 pt-8">{data.label}</div>
+            <div>
+              {data.items.map((item) => (
+                <Link key={item.label} {...item} className="block py-3.5 px-7 text-zinc-800 hover:text-blue-500 hover:bg-zinc-100 text-sm font-medium leading-tight">
+                  {item.label}
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <Link {...data} className={clsx("py-8 px-6 block", defaultClassName)}>
+      {data.label}
+    </Link>
+  )
+}
+
 export default function Navbar({ dark }: { dark?: boolean }): JSX.Element {
   const {
     navbar: { items },
@@ -49,7 +85,7 @@ export default function Navbar({ dark }: { dark?: boolean }): JSX.Element {
   const [leftItems, rightItems] = splitNavbarItems(items)
   // default class navbar
   return (
-    <nav className={clsx("px-6 py-2.5 sm:py-5 sm:px-7.5 flex flex-row justify-between space-x-2 items-center container mx-auto", dark && "dark")}>
+    <nav className={clsx("relative px-6 py-2.5 sm:py-5 sm:px-7.5 flex flex-row justify-between gap-x-2 items-center container mx-auto", dark && "dark")}>
       <span className="md:basis-1/3">
         <Logo />
       </span>
@@ -70,9 +106,24 @@ export default function Navbar({ dark }: { dark?: boolean }): JSX.Element {
         })}
       </span>
 
-      <button className="sm:hidden">
+      <input id="nav" type="checkbox" className="hidden peer" />
+      <label htmlFor="nav" className="sm:hidden click-area-4">
         <Hamburger className="dark:fill-white dark:fill-op-90" />
-      </button>
+      </label>
+
+      <div className="absolute top-full inset-x-0 grid grid-rows-[0fr] transition-all duration-300 peer-checked:grid-rows-[1fr] z-100">
+        <div className="overflow-hidden bg-white divide-solid divide-y-1">
+          {leftItems.map((item) => (
+            <MobileItem key={item.label} {...item} />
+          ))}
+
+          {rightItems.map((item) => (
+            <Link key={item.label} {...item} className="block p-6 ">
+              <div className="w-fit py-2.5 px-7 bg-zinc-800 rounded-sm text-white text-base font-medium leading-none">{item.label}</div>
+            </Link>
+          ))}
+        </div>
+      </div>
     </nav>
   )
 }
