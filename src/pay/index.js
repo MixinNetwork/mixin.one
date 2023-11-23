e7e2550b1849abd602aa4d20931ae1795ce49a5c
Hundredark <hundredark@gmail.com>
2023-11-23 23:04:05 +0800

    Fix asset count be empty in pay page (#627)

--- src/pay/index.js
+++ src/pay/index.js
@@ -11,6 +11,7 @@ import blueLogo from '../assets/icons/logo.png';
 import qrCodeIcon from '../assets/icons/qrcode.svg';
 import completeIcon from '../assets/icons/payment_complete.svg';
 import redirectingIcon from '../assets/icons/redirecting.svg';
+import transferAvatar from '../assets/icons/transferAvatar.svg';
 
 var validate = require('uuid-validate');
 
@@ -139,15 +140,12 @@ Pay.prototype = {
       return true;
     }
     self.api.network.assetsShow((resp) => {
-      if (resp.error && (resp.error.code === 400 || resp.error.code === 10002)) {
-        $('#layout-container').html(self.ErrorGeneral());
-        $('body').attr('class', 'error layout');
-        self.router.updatePageLinks();
-        return false;
-      }
+      if (resp.error && (resp.error.code === 404)) resp.error = undefined;
       const asset = resp.data;
-      var preloadImage = new Image();
-      preloadImage.src = asset.icon_url;
+      if (asset) {
+        var preloadImage = new Image();
+        preloadImage.src = asset.icon_url;
+      }
       const params = {
         isInitialized: false,
         address, asset, amount, traceId, memo
@@ -242,18 +240,14 @@ Pay.prototype = {
       }
 
       const payment = resp.data;
-      const useAmount = new Decimal(asset.price_usd).times(amount);
       const mixinURL = `https://mixin.one${window.location.pathname}${window.location.search}`;
       const data = {
         logoURL: blueLogo,
-        title: address,
+        title: i18n.t('schema.title.transfer'),
         hasSubTitle: true,
-        subTitle: i18n.t('schema.title.transfer'),
+        subTitle: address.slice(0, 6) + '...' + address.slice(-6),
         hasMemo: !!memo,
         memo,
-        iconUrl: asset.icon_url,
-        iconTitle: `${amount} ${asset.symbol}`,
-        iconSubTitle: `${useAmount.toNumber().toFixed(2).toString()} USD`,
         showQRCode: true,
         qrCodeIcon,
         tip: i18n.t('code.payment.mobile.scan'),
@@ -261,11 +255,27 @@ Pay.prototype = {
         mixinURL,
         redirectingIcon
       };
+      if (asset) {
+        const useAmount = new Decimal(asset.price_usd).times(amount);
+        data.iconUrl = asset.icon_url;
+        data.iconTitle = `${amount} ${asset.symbol}`;
+        data.iconSubTitle = `${useAmount.toNumber().toFixed(2).toString()} USD`;
+      } else {
+        data.basic = true;
+        data.hasAvatar = true;
+        data.avatarUrl = transferAvatar;
+      }
+
       if (!isInitialized) {
         params.isInitialized = true;
         let st = 'Pay ' + data.iconTitle  + ' to ' + address;
+        let bodyClass = 'pay code layout';
+        if (!asset) {
+          st = 'Pay to ' + address;
+          bodyClass += ' schema';
+        }
         $('title').html(st + ' | Mixin - Secure Digital Assets and Messages on Mixin');
-        $('body').attr('class', 'pay code layout');
+        $('body').attr('class', bodyClass);
         $('#layout-container').html(self.template(data));
         const platform = MixinUtils.environment();
         if (!platform) $('.main').attr('class', 'main browser');          
