0a14193e67ff106d13d042ca34b68d53ba6814bf
YeungKC <I@YeungKC.com>
2023-11-24 16:46:09 +0900

    Add className prop to LocaleLink component

--- new-website/src/components/common/LocaleLink.tsx
+++ new-website/src/components/common/LocaleLink.tsx
@@ -1,7 +1,8 @@
 import Link from "@docusaurus/Link"
 import useBaseUrl from "@docusaurus/useBaseUrl"
+import clsx from "clsx"
 
 export default function LocalLink(props: Parameters<typeof Link>[0]) {
   const to = useBaseUrl(props.to)
-  return <Link {...props} to={to} />
+  return <Link {...props} to={to} className={clsx(props.className, "block")} />
 }
