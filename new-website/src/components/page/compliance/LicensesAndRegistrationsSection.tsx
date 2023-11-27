81393428c8eca8227a148f3c5c7c8b23f402c335
YeungKC <I@YeungKC.com>
2023-11-27 15:07:44 +0900

    Update translations and descriptions in compliance section

--- new-website/src/components/page/compliance/LicensesAndRegistrationsSection.tsx
+++ new-website/src/components/page/compliance/LicensesAndRegistrationsSection.tsx
@@ -1,10 +1,10 @@
-import React from "react"
+import { ReactNode } from "react"
 import SectionTitle from "../../common/SectionTitle"
-import Translate from "@docusaurus/Translate"
+import Translate, { translate } from "@docusaurus/Translate"
 
 import clsx from "clsx"
 import GrayBackgroundWrapper from "../../common/GrayBackground"
-const Item = ({ cover, title, description }: { cover: string; title: string; description: string }) => (
+const Item = ({ cover, title, description }: { cover: string; title: string; description: ReactNode }) => (
   <div className={clsx("bg-white rounded-sm shadow pt-5 px-5 pb-10 md:p-15 lg:px-32 grid sm:grid-cols-2 group items-center gap-10")}>
     <img loading="lazy" src={require("@site/static/img/page/compliance/" + cover).default} alt={title} className="aspect-335/220 w-full object-contain px-6 sm:group-odd:order-last " />
 
@@ -24,16 +24,36 @@ export const LicensesAndRegistrationsSection = () => {
       <GrayBackgroundWrapper>
         <div className="py-15">
           <div className="container mx-auto px-5 grid gap-5 auto-rows-fr">
-            <Item cover="1.1.webp" title="Ministry of Finance" description="Registration of Activities in the Field of Virtual Currencies" />
+            <Item
+              cover="1.1.webp"
+              title={translate({
+                message: "Ministry of Finance",
+              })}
+              description={<Translate>Registration of Activities in the Field of Virtual Currencies</Translate>}
+            />
             <Item
               cover="1.2.webp"
-              title="PCI/DSS"
-              description="The Payment Card Industry Data Security Standard (PCI DSS) is an information security standard for organizations that handle branded credit cards from major card schemes. This certification confirms that Mixin Route is fully compliance with high standards for secure storage and processing of bank card data."
+              title={translate({
+                message: "PCI/DSS",
+              })}
+              description={
+                <Translate>
+                  The Payment Card Industry Data Security Standard (PCI DSS) is an information security standard for organizations that handle branded credit cards from major card schemes. This
+                  certification confirms that Mixin Route is fully compliance with high standards for secure storage and processing of bank card data.
+                </Translate>
+              }
             />
             <Item
               cover="1.3.webp"
-              title="EU GDPR compliance"
-              description="Mixin Route’s policies, procedures, and workflows regarding personal data processing of EU residents are fully compliance with the requirements of the EU GDPR. External and internal evaluations of Mixin Route's compliance with the EU GDPR are conducted annually."
+              title={translate({
+                message: "EU GDPR compliance",
+              })}
+              description={
+                <Translate>
+                  Mixin Route’s policies, procedures, and workflows regarding personal data processing of EU residents are fully compliance with the requirements of the EU GDPR. External and internal
+                  evaluations of Mixin Route's compliance with the EU GDPR are conducted annually.
+                </Translate>
+              }
             />
           </div>
         </div>
