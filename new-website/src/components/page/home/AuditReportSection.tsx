25246aade8935e0f2e10d25faa58d65a6af95a0d
YeungKC <I@YeungKC.com>
2023-11-27 14:51:13 +0900

    Refactor AuditReportSection component

--- new-website/src/components/page/home/AuditReportSection.tsx
+++ new-website/src/components/page/home/AuditReportSection.tsx
@@ -5,17 +5,15 @@ import LocalLink from "../../common/LocaleLink"
 export const AuditReportSection = () => (
   <div className="bg-zinc-100 bg-opacity-50">
     <div className="pt-20 sm:pt-50 pb-40 sm:pb-20 px-5 md:py-27 text-center md:text-start container mx-auto grid gap-y-20 gap-x-27.5 md:grid-cols-[474fr_356fr]">
-      <div className="flex flex-col justify-between md:py-12.5">
-        <div>
-          <div className="text-[#333] text-3xl font-medium leading-10">
-            <Translate>Security Audits</Translate>
-          </div>
-          <div className="mt-5 text-[#333] text-opacity-60 text-base font-normal leading-normal">
-            <Translate>We have an ongoing security audit process that keeps all Mixin infrastructures secured.</Translate>
-          </div>
+      <div className="flex flex-col justify-center md:py-12.5">
+        <div className="text-[#333] text-3xl font-medium leading-10">
+          <Translate>Security Audits</Translate>
+        </div>
+        <div className="mt-5 text-[#333] text-opacity-60 text-base font-normal leading-normal">
+          <Translate>We have an ongoing security audit process that keeps all Mixin infrastructures secured.</Translate>
         </div>
 
-        <LocalLink href="https://github.com/MixinNetwork/audits" className="hidden w-fit px-7 py-4 md:block bg-zinc-700 rounded-sm text-white font-medium leading-snug">
+        <LocalLink href="https://github.com/MixinNetwork/audits" className="hidden mt-24 w-fit px-7 py-4 md:block bg-zinc-700 rounded-sm text-white font-medium leading-snug">
           <Translate>View Audit Reports</Translate>
         </LocalLink>
       </div>
