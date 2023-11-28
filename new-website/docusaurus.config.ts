bbf40aec2196e48c765bca9003130b77f2db1790
YeungKC <I@YeungKC.com>
2023-11-28 10:51:54 +0900

    Update website URL and organization name

--- new-website/docusaurus.config.ts
+++ new-website/docusaurus.config.ts
@@ -25,15 +25,15 @@ const config: Config = {
   favicon: "img/favicon.png",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://Mixin.one/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "Mixin", // Usually your GitHub org/user name.
+  projectName: "Mixin.one", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
