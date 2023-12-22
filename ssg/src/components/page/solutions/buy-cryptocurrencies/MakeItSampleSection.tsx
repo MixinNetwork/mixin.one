ef6f128c6ef3a48f44c974812a18fad7a1827564
YeungKC <I@YeungKC.com>
2023-12-22 17:28:12 +0900

    Refactor buy-cryptocurrencies page

---
+++ ssg/src/components/page/solutions/buy-cryptocurrencies/MakeItSampleSection.tsx
@@ -0,0 +1,122 @@
+import React, { ReactNode } from "react"
+import Translate, { translate } from "@docusaurus/Translate"
+import SectionTitle from "../../../common/SectionTitle"
+import clsx from "clsx"
+
+const Item = ({
+  cover,
+  title,
+  description,
+  background,
+  className,
+}: {
+  cover: string
+  title: ReactNode
+  description: ReactNode
+  background: string
+  className?: string
+}) => {
+  return (
+    <div
+      className={clsx(
+        "bg-#F2F2F2 space-y-5 px-5 pb-8 pt-10",
+        "sm:h-166 sm:border-#333 sm:border-op-33 sm:relative sm:space-y-0 sm:rounded-lg sm:border sm:p-10",
+        "md:h-88.75",
+        "lg:h-110",
+        className,
+      )}
+    >
+      <img
+        src={
+          require(
+            "@site/static/img/page/solutions/buy-cryptocurrencies/" +
+              background,
+          ).default
+        }
+        className="fill op-0 sm:op-100"
+      />
+      <img
+        src={
+          require(
+            "@site/static/img/page/solutions/buy-cryptocurrencies/" + cover,
+          ).default
+        }
+        width={172}
+        height={172}
+        className={clsx(
+          "mx-auto",
+          "sm:w-43 sm:right-12.5 sm:absolute sm:bottom-5",
+        )}
+      />
+      <div className="sm:space-y-4.5 space-y-3 md:space-y-3">
+        <div className="text-5.5 sm:text-7 font-medium">{title}</div>
+        <div className="text-#333 text-op-80">{description}</div>
+      </div>
+    </div>
+  )
+}
+
+export const MakeItSampleSection = () => {
+  return (
+    <>
+      <SectionTitle
+        description={
+          <Translate>
+            Seamlessly move between fiat and crypto. lnexpensive and
+            hassle-free.
+          </Translate>
+        }
+        className="md:children:max-w-full md:children:w-full md:children:text-start md:mb-8 md:px-5"
+      >
+        <Translate>We make it simple</Translate>
+      </SectionTitle>
+      <div className="mx-a md:pb-55 container grid grid-flow-row auto-rows-fr gap-5 px-5 pb-40 md:grid-cols-2">
+        <Item
+          cover="2.1.webp"
+          background="2.5.webp"
+          title={translate({
+            message: "Lowest Fees",
+          })}
+          description={translate({
+            message:
+              "Kado allows you to spend what you wantwithout giving up an arm and a leg.",
+          })}
+          className="md:mt-15 sm:bg-white"
+        />
+        <Item
+          cover="2.2.webp"
+          background="2.6.webp"
+          title={translate({
+            message: "Fast & Reliable",
+          })}
+          description={translate({
+            message:
+              "Get crypto to your self-custody walletinstantly. Say goodbye to the long holdtimes & slippage on centralized exchanges.",
+          })}
+        />
+        <Item
+          cover="2.3.webp"
+          background="2.7.webp"
+          title={translate({
+            message: "Around The Globe",
+          })}
+          description={translate({
+            message:
+              "Available and fully compliant in 100+countries to give you access whereverand whenever.",
+          })}
+        />
+        <Item
+          cover="2.4.webp"
+          background="2.8.webp"
+          title={translate({
+            message: "Your Money How You Want It.",
+          })}
+          description={translate({
+            message: "Swap between fiat and crypto instantly.",
+          })}
+          className="md:-mt-15"
+        />
+      </div>
+    </>
+  )
+}
---
+++ ssg/src/pages/solutions/buy-cryptocurrencies/StepsSection.tsx
@@ -0,0 +1,74 @@
+import React, { ReactNode } from "react"
+import Translate from "@docusaurus/Translate"
+import clsx from "clsx"
+
+const Item = ({
+  stepName,
+  description,
+}: {
+  stepName: ReactNode
+  description: ReactNode
+}) => (
+  <div className="space-y-3">
+    <div className="text-5.5 md:text-6 font-medium uppercase">{stepName}</div>
+    <div className="text-#333 text-op-80">{description}</div>
+  </div>
+)
+
+export const StepsSection = () => {
+  return (
+    <div
+      className={clsx(
+        "gap-y-15 container mx-auto grid place-items-center px-5 pb-40",
+        "md:grid-cols-[470fr_450fr] md:place-items-start md:gap-x-10",
+        "lg:gap-x-38 lg:grid-cols-[536fr_507fr]",
+      )}
+    >
+      <img
+        src={
+          require("@site/static/img/page/solutions/buy-cryptocurrencies/3.1.webp")
+            .default
+        }
+        width={319}
+        height={483}
+        className="max-w-120 md:w-full"
+      />
+      <div className="md:mt-12">
+        <div className="text-#333 text-op-66">
+          <Translate>It’s less complicated than you think</Translate>
+        </div>
+        <div className="text-7 sm:text-8 md:text-9 mt-3 font-medium">
+          <Translate>Buy crypto in 4 simple steps</Translate>
+        </div>
+        <div className="mt-15 md:space-y-5.5 space-y-12">
+          <Item
+            stepName={<Translate>STEP 1</Translate>}
+            description={
+              <Translate>
+                Download Mixin Messenger and register an account, set up a PIN
+              </Translate>
+            }
+          />
+          <Item
+            stepName={<Translate>STEP 2</Translate>}
+            description={
+              <Translate>
+                Upload passport, ID card or driver’s license and complete KYC
+              </Translate>
+            }
+          />
+          <Item
+            stepName={<Translate>STEP 3</Translate>}
+            description={
+              <Translate>
+                {
+                  "Enter the amount, currency, select the payment method and complete the payment, your purchased cryptocurrency will soon enter your Mixin wallet."
+                }
+              </Translate>
+            }
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
