148307b3237c3ac85e335aaed6d443fa4d92cb37
YeungKC <I@YeungKC.com>
2023-12-29 14:31:44 +0900

    Refactor SectionTitle component

--- ssg/src/components/common/SectionTitle.tsx
+++ ssg/src/components/common/SectionTitle.tsx
@@ -6,30 +6,25 @@ export default function SectionTitle({
   className,
   titleClassName,
   descriptionClassName,
-  mode = "small",
 }: {
   children: React.ReactNode
   description?: React.ReactNode
   className?: string
   titleClassName?: string
   descriptionClassName?: string
-  mode?: "large" | "small"
 }) {
   return (
     <div
       className={clsx(
         "container mx-auto",
-        mode == "small" && "my-15 sm:mt-30 sm:mb-25 space-y-5",
-        mode == "large" && "my-15 space-y-10",
+        "my-15 sm:mt-30 sm:mb-25 space-y-5",
         className,
       )}
     >
       <div
         className={clsx(
           "title-container text-center",
-          mode == "small" && "text-7-12 font-medium leading-[1.4]",
-          mode == "large" &&
-            "text-8 sm:text-11 md:text-15 text-#333 font-medium",
+          "text-7-12 font-medium leading-[1.4]",
           titleClassName,
         )}
       >
@@ -39,8 +34,7 @@ export default function SectionTitle({
         <div
           className={clsx(
             "text-opacity-66 text-#333 leading-[1.5]",
-            mode == "small" && "title-container text-3.5-4 text-center",
-            mode == "large" && "title-container text-center",
+            "title-container text-3.5-4 text-center",
             descriptionClassName,
           )}
         >
