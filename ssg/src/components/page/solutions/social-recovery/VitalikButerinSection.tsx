fca95ecabee8fcf1e47b9018985a8a9e9c043688
YeungKC <I@YeungKC.com>
2024-01-02 17:43:48 +0900

    Refactor VitalikButerinSection component

--- ssg/src/components/page/solutions/social-recovery/VitalikButerinSection.tsx
+++ ssg/src/components/page/solutions/social-recovery/VitalikButerinSection.tsx
@@ -5,12 +5,12 @@ import QuotationMark from "@site/static/img/common/quotationMark.svg"
 
 export const VitalikButerinSection = () => (
   <WoodGrainBackgroundTextSection>
-    <div className="grid grid-cols-[auto_1fr_auto]">
+    <div className="mx-a grid w-fit grid-cols-[auto_1fr_auto]">
       <QuotationMark />
-      <div />
+      <div className="w-fit" />
       <QuotationMark className="rotate-y-180" />
       <div />
-      <div className="text-4 sm:text-4.7 md:text-5.3 lg:text-6 font-300 sm:font-333.3 md:font-366.7 lg:font-400">
+      <div className="text-4 sm:text-4.7 md:text-5.3 lg:text-6 font-300 sm:font-333.3 md:font-366.7 lg:font-400 mx-0-10 w-fit">
         <Translate>We need wide adoption of social recovery wallets.</Translate>
       </div>
       <div />
